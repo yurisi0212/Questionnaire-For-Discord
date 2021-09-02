86ad09c25e051c10e98284f0a6be8d2f85f689a0
yurisi0212 <Dev.yurisi@gmail.com>
2021-09-03 03:36:58 +0900

    プロジェクト ファイルを追加します。

---
+++ Questionnaire_For_Discord.Net/Program.cs
@@ -0,0 +1,52 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Questionnaire_For_Discord.Net {
+    class Program {
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        private TokenManager _token;
+
+        static void Main(string[] args) {
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task MainAsync() {
+            _client = new DiscordSocketClient(new DiscordSocketConfig {
+                LogLevel = LogSeverity.Info
+            });
+            _client.Log += Log;
+            _commands = new CommandService();
+            _services = new ServiceCollection().BuildServiceProvider();
+            _client.MessageReceived += CommandRecieved;
+            _token = new TokenManager();
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            await _client.LoginAsync(TokenType.Bot, _token.DiscordToken);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private async Task CommandRecieved(SocketMessage messageParam) {
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+            if (message.Author.IsBot) return;
+
+            var context = new CommandContext(_client, message);
+            var CommandContext = message.Content;
+
+        }
+
+        private Task Log(LogMessage message) {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ Questionnaire_For_Discord.Net/TokenManager.cs
@@ -0,0 +1,10 @@
+﻿namespace Questionnaire_For_Discord.Net {
+    public class TokenManager {
+
+        public TokenManager() {
+            DiscordToken = "";
+        }
+
+        public string DiscordToken { get; private set; }
+    }
+}
